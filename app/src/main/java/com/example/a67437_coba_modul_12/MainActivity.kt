edfd5bc451041b70a3cf1a17658e67f19e16b8bb
DaffaMuflih <daffa.muflih02@gmail.com>
2025-02-09 19:31:51 +0700

    Petak Favorite collections - Petak Lambat

--- app/src/main/java/com/example/a67437_coba_modul_12/MainActivity.kt
+++ app/src/main/java/com/example/a67437_coba_modul_12/MainActivity.kt
@@ -14,12 +14,16 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -182,9 +186,21 @@ fun AlignYourBodyRow(
 // Step: Favorite collections grid - LazyGrid
 @Composable
 fun FavoriteCollectionsGrid(
+    @DrawableRes drawable: Int = R.drawable.fc1_short_mantras,
+    @StringRes text: Int = R.string.fc1_short_mantras,
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text, Modifier.height(80.dp))
+        }
+    }
 }
 
 // Step: Home section - Slot APIs
@@ -287,7 +303,12 @@ fun FavoriteCollectionCardPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun FavoriteCollectionsGridPreview() {
-    _67437_cobamodul12Theme { FavoriteCollectionsGrid() }
+    _67437_cobamodul12Theme {
+        FavoriteCollectionsGrid(
+            drawable = R.drawable.fc1_short_mantras,
+            text = R.string.fc1_short_mantras
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
